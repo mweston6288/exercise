a666a3982675e5dd6b744832e591999a729e4db1
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-04-15 17:02:33 -0400

    Still working on getting exercises in. Now the front and backend agree on what the last workout is

--- routes/api.js
+++ routes/api.js
@@ -1,26 +1,24 @@
 const router = require("express").Router();
-const Workout = require("../models/Workout.js");
-
+const db = require("../models/");
 
 router.get("/api/workouts", (req, res) => {
-
-    Workout.find({}).sort().then((data) => {
+    db.Workout.find({}).sort({"day": -1}).limit(1).then((data) => {
+        console.log(data)
         res.json(data);
     });
 });
 router.post("/api/workouts", (req,res) =>{
     console.log(req.body);
-    Workout.create(req.body);
+    db.Workout.create(req.body);
 });
 router.put("/api/workouts/:id", (req, res) => {
-
-    Workout.update(req.body, (error, data) => {
-        if (error) {
-            res.send(error);
-        } else {
-            res.send(data);
+    if (req.params){
+        db.Exercises.create(req.body)
+            .then(({ _id })=> db.Workout.findOneAndUpdate({_id : req.params}, { $push: { exercises: _id } }))
+            .then(WorkoutDB=>{
+                res.json(WorkoutDB);
+            })
         }
-    })
 });
 router.get("/api/workouts/range", (req, res) => {
 })
