94dbbe1a5ab3d53ed1efdb218e6e171f3c326493
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-04-16 10:06:31 -0400

    All routes are finished. All that's left is getting the stats page to work

--- routes/api.js
+++ routes/api.js
@@ -5,25 +5,27 @@ const mongojs = require("mongojs");
 
 router.get("/api/workouts", (req, res) => {
     db.Workout.find({}).sort({"day": -1}).limit(1).then((data) => {
-        console.log(data)
         res.json(data);
     });
 });
 router.post("/api/workouts", (req,res) =>{
-    console.log(req.body);
-    db.Workout.create(req.body);
+    db.Workout.create(req.body).then(data=>{
+        res.json(data);
+    })
 });
 router.put("/api/workouts/:id", (req, res) => {
-    if (req.params){
-        console.log(req.body)
+    if (req.params.id != undefined){
         db.Exercises.create(req.body)
             .then(({ _id })=> db.Workout.findOneAndUpdate({"_id" : mongojs.ObjectId(req.params.id)}, { $push: { exercises: _id }, $inc:{ "totalDuration": req.body.duration} }))
             .then(WorkoutDB=>{
                 res.json(WorkoutDB);
             })
-        }
+    }
 });
 router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({}).then((data)=>{
+        res.json(data);
+    })
 })
 
 module.exports = router;
